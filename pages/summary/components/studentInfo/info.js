cdc40106c519b9052249d6b2c2b639aaff0aa6d0
unknown <linlianghao@ad61v1.com>
2019-09-06 10:05:14 +0800

    新增：学习阶段总结页面

---
+++ pages/summary/components/studentInfo/info.js
@@ -0,0 +1,34 @@
+// pages/summary/components/studentInfo/info.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imageUrl: {
+      banner: 'http://10.10.117.199:3000/images/summary-info-banner.png',
+      arrow: 'http://10.10.117.199:3000/images/summary-info-arrow.png'
+    },
+    name: '孔维浩',
+    headImage: 'http://10.10.117.199:3000/images/headImage.png',
+    level: 1,
+    stage: 1,
+    worksTotal: 40,
+    worksAdd: 12,
+    timeTotal: 2386,
+    timeAdd: 1536
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
