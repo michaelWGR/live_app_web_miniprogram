e036759f1545ed7417de3827b61de2340549e928
unknown <linlianghao@ad61v1.com>
2019-09-07 09:17:58 +0800

    暂存

--- pages/summary/components/promote/promote.js
+++ pages/summary/components/promote/promote.js
@@ -1,4 +1,4 @@
-// pages/summary/components/title/title.js
+// pages/summary/components/promote/promote.js
 Component({
   /**
    * 组件的属性列表
@@ -11,7 +11,12 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    image: {
+      title: 'http://10.10.117.199:3000/images/summary-promote-title.png'
+    },
+    add: 8,
+    accumulate: 15,
+    total: 24
   },
 
   /**
---
+++ pages/summary/components/stage/stage.js
@@ -0,0 +1,30 @@
+// pages/summary/components/stage/stage.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    image: {
+      title: 'http://10.10.117.199:3000/images/summary-stage-title.png',
+      crown: 'http://10.10.117.199:3000/images/summary-stage-crown.png',
+      cloud: 'http://10.10.117.199:3000/images/summary-stage-cloud.png'
+    },
+    level: 1,
+    stage: 2,
+    stageList: ['线条刻画', '图形装饰', '色彩搭配', '构图意识']
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
