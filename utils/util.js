8290085d8e54e739502fb83fa4261146de656ea9
fangjiapeng <1587510315@qq.com>
2019-09-12 14:00:50 +0800

    修改图片地址

--- utils/util.js
+++ utils/util.js
@@ -2,7 +2,7 @@
 // const BASE_URL = "https://appminip-test.61draw.com" //测试线地址
 const BASE_URL = "https://appminip.61draw.com"         //正式线地址
 
-const IMG_BASEURL = "http://10.10.117.177:3000/images/"; //本地图片地址
+const IMG_BASEURL = "http://10.10.117.177:3000/dev_test/miniprogram/"; //本地图片地址
 // const IMG_BASEURL = "https://appdev.61draw.com/dev_test/miniprogram/"  //测试线图片地址
 // const IMG_BASEURL = "http://appminip.61draw.com/res/images/"; //正式线图片地址
 
