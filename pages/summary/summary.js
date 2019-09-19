f20910f9081a9df4a1c0854620fe933788e245e1
unknown <linlianghao@ad61v1.com>
2019-09-19 19:48:35 +0800

    修改：分享后又弹框

--- pages/summary/summary.js
+++ pages/summary/summary.js
@@ -29,7 +29,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const userId =  58661
+    const userId = 6322
     const levelStage = {
       level: 1,
       stage: 1
@@ -83,6 +83,7 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    this.goToOtherPage()
     return {
       title: this.data.userInfo.nickname + '《Level ' + this.data.levelStage.level + ' stage ' + this.data.levelStage.stage + '》的画啦啦艺术成长报告'
     }
